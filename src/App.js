eff2e484a821120fd7634c1efc13419499b44139
gonimy <mhmodgonim96@gmail.com>
2020-11-25 11:52:36 +0200

    Done with the project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { OurWork } from "./pages/OurWork";
 import { AboutUs } from "./pages/AboutUs";
 
 import { Switch, Route } from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
 
 function App() {
   return (
@@ -15,8 +16,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetails />
         </Route>
         <Route path="/contact">
           <ContactUs />
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,9 +1,58 @@
 import React from "react";
 
+import athlete2 from "../img/athlete2.png";
+import theracer from "../img/theracer.png";
+import goodtimes2 from "../img/goodtimes2.jpg";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Worlk = styled.div`
+  min-height: 100vh;
+  color: white;
+  padding: 5rem 10rem;
+  overflow: hidden;
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    object-fit: cover;
+    width: 100%;
+    height: 70vh;
+  }
+`;
+
 export const OurWork = () => {
   return (
-    <div>
-      <h1>working</h1>
-    </div>
+    <Worlk>
+      <Movie>
+        <h2>The Athlete </h2>
+        <div className="line"></div>
+        <Link to="/work/the-athlete">
+          <img src={athlete2} alt="" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times </h2>
+        <div className="line"></div>
+        <Link to="/work/good-times">
+          <img src={goodtimes2} alt="" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer </h2>
+        <div className="line"></div>
+        <Link to="/work/the-racer">
+          <img src={theracer} alt="" />
+        </Link>
+      </Movie>
+    </Worlk>
   );
 };
